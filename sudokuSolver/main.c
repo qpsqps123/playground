f156d04e934709fdebb5db0def3a2ffc436cc52a
qpsqps123 <qpsqps321@gmail.com>
2022-03-01 20:02:37 +0900

    got ya two, argument-count and argument vector

--- sudokuSolver/main.c
+++ sudokuSolver/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-#include <io.h> // <io.h> = <unistd.h> in VSC
-
+#include <unistd.h> // <io.h> = <unistd.h> in VSC
+//int argc, char*argv[]
 int main(int argc, char*argv[])
 {
     int i;
-    char j[10]
-    j = "Hello World";
+    char j[15] = "Hello World\n";
+    write(1, j, 12);
+    printf("argc = %d개\n", argc);
+    for (i = 0; i < argc; i++)
+    {
+        printf("argv[%d] = %s 입니다.\n",i,argv[i]);
+    }
     
-    write(1,j,5);
-    printf("argc = %d", argc);
-
-    //for()
 	
     return 0;
 }
