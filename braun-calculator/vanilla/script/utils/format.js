4b094565e4d878c2b842b39d146273fd2e69ccff
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-08-06 19:53:28 +0900

    [braun][fix] displayValue가 10의 자리가 넘어가면 오류가 나던 버그 픽스

--- braun-calculator/vanilla/script/utils/format.js
+++ braun-calculator/vanilla/script/utils/format.js
@@ -1,6 +1,6 @@
 export function formatValue(value) {
   if (value.toString().length > 10) {
-    exponentialForm = value.toExponential(4);
+    const exponentialForm = value.toExponential(4);
     return exponentialForm;
   } else {
     console.log("걸렸다 요놈");
