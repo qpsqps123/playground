74f17975902164d354a32c67b3461d103a9ee7a5
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-13 13:32:25 +0900

    [chore] lab폴더 이름 변경 및 braun-calculator 프로젝트를 portfolio 폴더로 이동
    - lab폴더가 github repository에 이미 있으므로, 중복 및 혼선 방지를 위해 이름을 experiment로 변경.

--- braun-calculator/vanilla/script/reset/index.js
+++
@@ -1,2 +0,0 @@
-export * from "./clearEntry.js";
-export * from "./clear.js";
--- braun-calculator/vanilla/script/utils/format.js
+++
@@ -1,9 +0,0 @@
-export function formatValue(value) {
-  if (value.toString().length > 10) {
-    const exponentialForm = value.toExponential(4);
-    return exponentialForm;
-  } else {
-    console.log("걸렸다 요놈");
-    return value;
-  }
-}
