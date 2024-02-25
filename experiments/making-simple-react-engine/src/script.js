ebd6f598e2c3a104d07416f4c51cf3db303d7dcd
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-02-25 23:33:30 +0900

    [chore] experiments 저장소 생성에 따른 폴더 및 파일 이동

--- experiments/making-simple-react-engine/src/script.js
+++
@@ -1,9 +0,0 @@
-import createElement from "./utils/createElement.js";
-import renderElement from "./utils/renderElement.js";
-
-const list = createElement("li", "zzz");
-
-const p = createElement("p", "hehehe");
-
-renderElement("#root", "beforeend", list);
-renderElement("#root", "beforeend", p);
--- experiments/making-simple-react-engine/src/utils/renderElement.js
+++
@@ -1,4 +0,0 @@
-// 1. 이전 상태와 현재 상태를 비교하는 내용을 포함한다(Fiber처럼).
-// 2. createElement와 useState를 사용한다.
-// 3. 실제로 렌더링하는 로직을 포함한다. innerHTML을 사용하는 방법을 생각해본다.
-export default function renderElement() {}
